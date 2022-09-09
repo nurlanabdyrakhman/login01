84adc58365c34f405d1d817bdfaa75d990dada64
Darkan Mamazhanova <nurlanabdyrakhman>
2022-09-09 20:07:29 +0300

    22.09.09

--- lib/controllers/data_uploader.dart
+++ lib/controllers/data_uploader.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'dart:convert';
 
 class DataUploader extends GetxController {
   @override
@@ -7,7 +10,17 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {
-    print('Data is uploading');
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifesMap = json.decode(manifestContent);
+    final papersInAssets = manifesMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/paper") && path.contains('.json'))
+        .toList();
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      print(stringPaperContent);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:login01/constants/data_uploader_screen.dart';
 import 'package:login01/firebase_options.dart';
 import 'package:login01/screens/auth-screen.dart';
 import 'package:login01/screens/overview_screen.dart';
 import 'package:login01/screens/splash_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  //WidgetsFlutterBinding.ensureInitialized();
+//  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp( GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:DataUploaderCcreen()));
 }
 
 class MyApp extends StatelessWidget {
